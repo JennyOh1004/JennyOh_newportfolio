381eb2837b39e5257c36a44d2599179df10fe985
Jenny Oh <jennyoh@Jennyui-MacBook-Pro.local>
2020-06-25 20:45:45 -0700

    Updated metif korea project html file

--- js/index.js
+++ js/index.js
@@ -12,3 +12,21 @@ window.addEventListener("scroll", (e) => {
     header.classList.remove("scrolled");
   }
 });
+
+//On Scroll Effect
+
+function animateOnScroll(target, triggerPosition, reversible = false) {
+  let targetEl = document.querySelectorAll(target);
+  targetEl.forEach((el) => {
+    let targetElTop = el.getBoundingClientRect().top;
+    let windowHeight = window.innerHeight / 1.5;
+    if (targetElTop <= windowHeight * triggerPosition) {
+      el.classList.add("intro_appear");
+    } else if (targetElTop >= windowHeight && reversible) {
+      el.classList.remove("intro_appear");
+    }
+  });
+}
+window.addEventListener("scroll", () => {
+  animateOnScroll(".intro_text", 0.8, true);
+});
